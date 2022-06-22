33909e674247d509b177b16d98fefb5dac5677c5
Felipe Alves <felipe.souza@timeguilu.com.br>
2022-06-22 19:31:09 -0300

    Add project files.

---
+++ ContaBancaria.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    public class ContaBancaria
+    {
+        private int numeroConta;
+        public int NumeroConta { get; private set; }
+        public string NomeTitularConta { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria()
+        {
+
+        }
+
+        public ContaBancaria(int numeroConta, string nomeTitular)
+        {
+            NumeroConta = numeroConta;
+            NomeTitularConta = nomeTitular;
+        }
+
+        public ContaBancaria(int numeroConta, string nomeTitular, double depositoInicial) : this(numeroConta, nomeTitular)
+        {
+            Deposito(depositoInicial);
+        }
+
+
+        public void Deposito(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void Saque(double saque)
+        {
+            Saldo -= saque + 5;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta: {NumeroConta}, Titular: {NomeTitularConta}, Saldo: $ {Saldo.ToString("F2")}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+            Console.WriteLine("Bem vindo ao Banco !!!");
+            Console.WriteLine("Vamos criar uma conta!");
+
+            Console.Write("Digite o numero da conta que deseja criar: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Digite o nome do titular da conta que deseja criar: ");
+            string nomeTitular = Console.ReadLine();
+
+
+            Console.Write("Digite Sim ou Nao se deseja fazer depósito inicial: ");
+            string opcao = Console.ReadLine();
+
+
+            if (opcao == "Sim")
+            
+            {
+                Console.Write("Digite o valor: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                conta = new ContaBancaria(numeroConta , nomeTitular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numeroConta, nomeTitular);
+            }
+            
+            Console.WriteLine(conta);
+
+            Console.Write("Coloque o valor para depositar em conta: ");
+            double deposito = double.Parse(Console.ReadLine());
+            conta.Deposito(deposito);
+
+            Console.WriteLine($"Informações atualizadas após o depósito: {conta}");
+
+
+            Console.Write("Coloque o valor para saque em conta: ");
+            double saque = double.Parse(Console.ReadLine());
+            conta.Saque(saque);
+            
+            Console.WriteLine($"Informações atualizadas após o saque: {conta}");
+            
+        }
+
+    }
+}
